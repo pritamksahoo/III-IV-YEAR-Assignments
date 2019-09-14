d27e1c148868cb0735bb06777dffc837de946d98
pritamksahoo <pritam.ndp@gmail.com>
2019-09-14 23:43:24 +0530

    pks

--- string_sort.py
+++ string_sort.py
@@ -10,21 +10,24 @@ def string_sort(arr, length):
 	# Considering last character and sort on the basis
 	for j in range(length):
 		string = arr[j]
-		temp1[ascii(string[-1])].append(string)
+		if len(string) == max_len:
+			temp1[ascii(string[-1])].append(string)
+		else:
+			temp1[0].append(string)
 
 	# Moving from the last character one by one and sorting on basis of them
-	for i in range(1, max_len+1):
+	for i in range(1, max_len):
 		temp2 = [list() for i in range(27)]
 		
 		for j in range(27):
 			for string in temp1[j]:
-				if i < len(string):
-					temp2[ascii(string[-1-i])].append(string)
+				if len(string) >= max_len-i:
+					temp2[ascii(string[max_len-i-1])].append(string)
 				else:
-					temp2[j].append(string)
+					temp2[0].append(string)
 
 		temp1 = temp2.copy()
-
+		
 	# Finally coping the result to arr
 	arr = []
 	for j in range(27):
