1b3f2cdf9d33ecebc4351efec129190966f3e38d
pritamksahoo <pritam.ndp@gmail.com>
2019-09-27 20:18:11 +0530

    SNS Updates

--- SNS/des.cpp
+++ SNS/des.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 string DecToBinary(int num[], int start, int end);
 void KeyGen(bitset<56> key, bitset<48> round_key, int round);
-void encrypt(bitset<64> bs_plain, bitset<56> key);
+void encrypt(bitset<64> bs_plain, bitset<64> key);
 void mixer(bitset<32> input, bitset<48> round_key, bitset<32> output);
 
 template<size_t N>
@@ -168,7 +168,9 @@ void KeyGen(bitset<56> key, bitset<48> round_key, int round)
 		key[i++] = bs2[j];
 	}
 
-	int CP[] = {};
+	int CP[] = {
+
+	};
 
 	permutation(key, round_key, CP);
 
@@ -178,7 +180,16 @@ void mixer(bitset<32> input, bitset<48> round_key, bitset<32> output)
 {
 	bitset<48> temp_input;
 	bitset<32> temp_output;
-	int ED = {};
+	int ED = {
+		32,  1,  2,  3,  4,  5,
+	   	4,  5,  6,  7,  8,  9,
+	   	8,  9, 10, 11, 12, 13,
+	  	12, 13, 14, 15, 16, 17,
+	  	16, 17, 18, 19, 20, 21,
+	  	20, 21, 22, 23, 24, 25,
+	  	24, 25, 26, 27, 28, 29,
+	  	28, 29, 30, 31, 32,  1
+	};
 	
 	permutation(input, temp_input, ED);
 
@@ -188,15 +199,57 @@ void mixer(bitset<32> input, bitset<48> round_key, bitset<32> output)
 
 	substitution(temp_input, temp_output, S);
 
-	int P[] = {};
+	int P[] = {
+		16,  7, 20, 21,
+	  	29, 12, 28, 17,
+	   	1, 15, 23, 26,
+	   	5, 18, 31, 10,
+	   	2,  8, 24, 14,
+	  	32, 27,  3,  9,
+	  	19, 13, 30,  6,
+	  	22, 11,  4, 25
+	};
 
 	permutation(temp_output, output, P);
 }
 
-void encrypt(bitset<64> bs_plain, bitset<56> key)
+void encrypt(bitset<64> bs_plain, bitset<64> key_org)
 {
-	int IP[] = {};
-	int FP[] = {};
+	biset<56> key;
+	int CP[] = {
+		57,49,41,33,25,17,9, 
+        1,58,50,42,34,26,18, 
+        10,2,59,51,43,35,27, 
+        19,11,3,60,52,44,36,           
+        63,55,47,39,31,23,15, 
+        7,62,54,46,38,30,22, 
+        14,6,61,53,45,37,29, 
+        21,13,5,28,20,12,4
+	};
+
+	// Parity Drop
+	permutation(key_org, key, CP);
+
+	int IP[] = {
+		58, 50, 42, 34, 26, 18, 10, 2,
+	  	60, 52, 44, 36, 28, 20, 12, 4,
+	  	62, 54, 46, 38, 30, 22, 14, 6,
+	  	64, 56, 48, 40, 32, 24, 16, 8,
+	  	57, 49, 41, 33, 25, 17,  9, 1,
+	  	59, 51, 43, 35, 27, 19, 11, 3,
+	  	61, 53, 45, 37, 29, 21, 13, 5,
+	  	63, 55, 47, 39, 31, 23, 15, 7
+	};
+	int FP[] = {
+		40, 8, 48, 16, 56, 24, 64, 32,
+	  	39, 7, 47, 15, 55, 23, 63, 31,
+	  	38, 6, 46, 14, 54, 22, 62, 30,
+	  	37, 5, 45, 13, 53, 21, 61, 29,
+	  	36, 4, 44, 12, 52, 20, 60, 28,
+	  	35, 3, 43, 11, 51, 19, 59, 27,
+	  	34, 2, 42, 10, 50, 18, 58, 26,
+	  	33, 1, 41,  9, 49, 17, 57, 25
+	};
 	bitset<64> bs_int;
 	bitset<48> round_key;
 	permutation(bs_plain, bs_int, IP);
@@ -210,7 +263,7 @@ void encrypt(bitset<64> bs_plain, bitset<56> key)
 		int j = 0;
 		for (j=0; j<33; j++)
 		{
-			l[i] = bs_int[i];
+			l[j] = bs_int[j];
 		}
 		for(int k=0; k<33; k++)
 		{
