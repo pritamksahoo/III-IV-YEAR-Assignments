a866b4021308e75d593110aa2d533247af935dac
pritamksahoo <pritam.ndp@gmail.com>
2019-09-30 13:59:24 +0530

    SNS Updates

--- SNS/des.cpp
+++ SNS/des.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 #define MAX 1000
+bitset<48> global_round_key[16];
 
 string DecToBinary(int num[], int start, int end);
 void KeyGen(bitset<56>& key, bitset<48>& round_key, int round);
 void encrypt(bitset<64>& bs_plain, bitset<64>& key);
+void decrypt(bitset<64>& bs_plain);
 void mixer(bitset<32>& input, bitset<48>& round_key, bitset<32>& output);
 
 template<size_t N>
@@ -87,61 +89,83 @@ void substitution(bitset<N1>& bs1, bitset<N2>& bs2, int s_box[8][4][16])
 
 int main()
 {
-	// string P, K;
-	// int plain[MAX], key[8];
-	// cin >> P;
-	// cin >> K;
+	string P, K;
+	int plain[MAX], key[8], cipher[MAX];
+	cin >> P;
+	cin >> K;
 
-	// int len = P.length(), index = 0, key_index = 0;
-	// for (int i=0; i<len; i++)
-	// {
-	// 	if (P[i] != ' ')
-	// 	{
-	// 		plain[index++] = (int)(P[i]);
-	// 	}
-	// }
-	// int rem = 8 - index%8;
-	// if (rem != 8)
-	// {
-	// 	for (int i=0; i<rem; i++)
-	// 	{
-	// 		plain[index++] = 0;
-	// 	}
-	// }
+	int len = P.length(), index = 0, key_index = 0;
+	for (int i=0; i<len; i++)
+	{
+		if (P[i] != ' ')
+		{
+			plain[index++] = (int)(P[i]);
+		}
+	}
+	int rem = 8 - index%8;
+	if (rem != 8)
+	{
+		for (int i=0; i<rem; i++)
+		{
+			plain[index++] = 0;
+		}
+	}
 
-	// len = K.size();
-	// for (int i=0; i<len; i++)
-	// {
-	// 	if (K[i] != ' ')
-	// 	{
-	// 		key[key_index++] = (int)(K[i]);
-	// 	}
-	// 	if (key_index == 8)
-	// 	{
-	// 		break;
-	// 	}
-	// }
+	len = K.size();
+	for (int i=0; i<len; i++)
+	{
+		if (K[i] != ' ')
+		{
+			key[key_index++] = (int)(K[i]);
+		}
+		if (key_index == 8)
+		{
+			break;
+		}
+	}
 
-	// bitset<64> key_org (DecToBinary(key, 0, 7));
+	bitset<64> key_org (DecToBinary(key, 0, 7));
+	int cipher_index = 0;
 
-	// for (int i=0; i<index; i=i+8)
-	// {
-	// 	bitset<64> plain_org (DecToBinary(plain, i, i+7));
-	// 	encrypt(plain_org, key_org);
-	// 	cout << plain_org;
-	// }
-	// cout << "" << endl;
+	for (int i=0; i<index; i=i+8)
+	{
+		bitset<64> plain_org (DecToBinary(plain, i, i+7));
+		encrypt(plain_org, key_org);
+		cout << plain_org;
+		
+		bitset<8> temp_cipher;
+		for (int j=63; j>0; j=j-8)
+		{
+			for (int k=0; k<8; k++)
+			{
+				temp_cipher[k] = plain_org[j-7+k];
+			}
+			cipher[cipher_index++] = BinaryToDec(temp_cipher);
+		}
+		
+	}
+
+	cout << endl;
+
+	for (int i=0; i<index; i=i+8)
+	{
+		bitset<64> cipher_org (DecToBinary(cipher, i, i+7));
+		decrypt(cipher_org);
+		cout << cipher_org;
+	}
+
 	// for (int i=0; i<index; i++)
 	// {
 	// 	int arr[] = {plain[i]};
 	// 	cout << DecToBinary(arr,0,0);
 	// }
 	
-	bitset<64> plain (string("0000000100100011010001010110011110001001101010111100110111101111"));
-	bitset<64> key (string("0001001100110100010101110111100110011011101111001101111111110001"));
-	encrypt(plain, key);
-	cout << plain << endl;
-
+	// bitset<64> plain (string("0000000100100011010001010110011110001001101010111100110111101111"));
+	// bitset<64> key (string("0001001100110100010101110111100110011011101111001101111111110001"));
+	// encrypt(plain, key);
+	// cout << plain << endl;
+	// decrypt(plain);
+	// cout << plain;
 }
 
 string DecToBinary(int num[], int start, int end)
@@ -351,12 +375,13 @@ void encrypt(bitset<64>& bs_plain, bitset<64>& key_org)
 	bitset<64> bs_int;
 	bitset<48> round_key;
 	permutation(bs_plain, bs_int, IP);
-	cout << "After initial permutation : " << bs_int << endl;
+	// cout << "After initial permutation : " << bs_int << endl;
 
 	for (int i=1; i<=16; i++)
 	{
 		// Round Key
 		KeyGen(key, round_key, i);
+		global_round_key[i-1] = round_key;
 		// cout << "Round #" << i << " : " << endl;
 		bitset<32> feistel, l, r;
 		
@@ -392,6 +417,76 @@ void encrypt(bitset<64>& bs_plain, bitset<64>& key_org)
 	}
 
 	ShiftLeft(bs_int, 32);
+
+	permutation(bs_int, bs_plain, FP);
+
+}
+
+void decrypt(bitset<64>& bs_plain)
+{
+	int IP[] = {
+		57, 49, 41, 33, 25, 17,  9,  1,
+       	59, 51, 43, 35, 27, 19, 11,  3,
+       	61, 53, 45, 37, 29, 21, 13,  5,
+       	63, 55, 47, 39, 31, 23, 15,  7,
+       	56, 48, 40, 32, 24, 16,  8,  0,
+       	58, 50, 42, 34, 26, 18, 10,  2,
+       	60, 52, 44, 36, 28, 20, 12,  4,
+       	62, 54, 46, 38, 30, 22, 14,  6
+	};
+	int FP[] = {
+		39,  7, 47, 15, 55, 23, 63, 31,
+       	38,  6, 46, 14, 54, 22, 62, 30,
+       	37,  5, 45, 13, 53, 21, 61, 29,
+       	36,  4, 44, 12, 52, 20, 60, 28,
+       	35,  3, 43, 11, 51, 19, 59, 27,
+       	34,  2, 42, 10, 50, 18, 58, 26,
+       	33,  1, 41,  9, 49, 17, 57, 25,
+       	32,  0, 40,  8, 48, 16, 56, 24
+	};
+
+	bitset<64> bs_int;
+	permutation(bs_plain, bs_int, IP);
+	ShiftLeft(bs_int, 32);
+
+	for (int i=15; i>=0; i--)
+	{
+		// cout << "Round #" << i << " : " << endl;
+		bitset<32> feistel, l, r;
+		
+		// Swapper
+		ShiftLeft(bs_int, 32);
+
+		int j = 0;
+		for (j=0; j<32; j++)
+		{
+			r[j] = bs_int[j];
+		}
+		for(int k=0; k<32; k++)
+		{
+			l[k] = bs_int[j++];
+		}		
+
+		// cout << "l(old) : " << l << endl;
+		// cout << "r(old) : " << r << endl;
+		// Mixer
+		mixer(r, global_round_key[i], feistel);
+		// cout << "global_round_key : " << global_round_key[i] << endl;
+		l ^= feistel;
+		// cout << "New l : " << l << endl;
+
+		// Joining
+		j = 0;
+		for (int k=0; k<32; k++)
+		{
+			bs_int[j++] = r[k];
+		}
+		for (int k=0; k<32; k++)
+		{
+			bs_int[j++] = l[k];
+		}
+		// cout << "Finally : " << bs_int << endl;
+	}
 
 	permutation(bs_int, bs_plain, FP);
 }
--- SNS/des3_openssl.cpp
+++ SNS/des3_openssl.cpp
@@ -3,53 +3,15 @@
 #include <string.h>
 #include <openssl/des.h>
 
-/* Triple DES key for Encryption and Decryption */
-DES_cblock Key1 = { 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11 };
-DES_cblock Key2 = { 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22 };
-DES_cblock Key3 = { 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33 };
-DES_key_schedule SchKey1,SchKey2,SchKey3;
+/*
+Run the code by typing in terminal the following command :
+g++ des3_openssl.cpp -lssl -lcrypto
+./a.out
+*/
 
 /* Print Encrypted and Decrypted data packets */
-void print_data(const char *tittle, const void* data, int len);
-
-int main()
+void print(const void* data, int len)
 {
-    /* Input data to encrypt */
-    unsigned char input_data[] = {0x01, 0x02, 0x03, 0x04, 0x05};
-    
-    /* Init vector */
-    DES_cblock iv = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-    DES_set_odd_parity(&iv);
-    
-    /* Check for Weak key generation */
-    if ( -2 == (DES_set_key_checked(&Key1, &SchKey1) || DES_set_key_checked(&Key2, &SchKey2) || DES_set_key_checked(&Key3, &SchKey3)))
-    {
-        printf(" Weak key ....\n");
-        return 1;
-    }
-    
-    /* Buffers for Encryption and Decryption */
-    unsigned char* cipher[sizeof(input_data)];
-    unsigned char* text[sizeof(input_data)];
-    
-    /* Triple-DES CBC Encryption */
-    DES_ede3_cbc_encrypt( (unsigned char*)input_data, (unsigned char*)cipher, sizeof(input_data), &SchKey1, &SchKey2, &SchKey3,&iv, DES_ENCRYPT);
-    
-    /* Triple-DES CBC Decryption */
-    memset(iv,0,sizeof(DES_cblock)); // You need to start with the same iv value
-    DES_set_odd_parity(&iv);
-    DES_ede3_cbc_encrypt( (unsigned char*)cipher, (unsigned char*)text, sizeof(input_data), &SchKey1, &SchKey2, &SchKey3,&iv,DES_DECRYPT);
-    
-    /* Printing and Verifying */
-    print_data("\n Original ",input_data,sizeof(input_data));
-    print_data("\n Encrypted",cipher,sizeof(input_data));
-    print_data("\n Decrypted",text,sizeof(input_data));
-    
-    return 0;
-}
-void print_data(const char *tittle, const void* data, int len)
-{
-    printf("%s : ",tittle);
     const unsigned char * p = (const unsigned char*)data;
     int i = 0;
     
@@ -57,4 +19,49 @@ void print_data(const char *tittle, const void* data, int len)
         printf("%02X ", *p++);
     
     printf("\n");
+}
+
+int main()
+{
+    // Hexadecimal input
+    unsigned char input_data[] = {0xf1, 0xa2, 0x03, 0xb4, 0x05};
+    // Buffers for storing cipher (After encryption) and plain text (After decryption)
+    unsigned char* cipher[sizeof(input_data)];
+    unsigned char* text[sizeof(input_data)];
+    // Initial vector
+    DES_cblock iv = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    DES_set_odd_parity(&iv);
+
+    // Triple DES key for Encryption and Decryption 
+    DES_cblock Key1 = { 0x11, 0x22, 0x11, 0x11, 0x31, 0x11, 0x01, 0x11 };
+    DES_cblock Key2 = { 0x22, 0xb2, 0x22, 0x22, 0x2d, 0x22, 0x22, 0x22 };
+    DES_cblock Key3 = { 0x33, 0x13, 0x33, 0x33, 0x03, 0x33, 0x3e, 0x33 };
+    DES_key_schedule SchKey1,SchKey2,SchKey3;
+    
+    /* Check for Weak key generation */
+    if ((DES_set_key_checked(&Key1, &SchKey1) || DES_set_key_checked(&Key2, &SchKey2) || DES_set_key_checked(&Key3, &SchKey3)) == -2)
+    {
+        printf("Weak key ....\n");
+        return 1;
+    }
+    else
+    {
+        // Triple-DES CBC Encryption 
+        DES_ede3_cbc_encrypt( (unsigned char*)input_data, (unsigned char*)cipher, sizeof(input_data), &SchKey1, &SchKey2, &SchKey3,&iv, DES_ENCRYPT);
+        
+        // Triple-DES CBC Decryption 
+        memset(iv,0,sizeof(DES_cblock));
+        DES_set_odd_parity(&iv);
+        DES_ede3_cbc_encrypt( (unsigned char*)cipher, (unsigned char*)text, sizeof(input_data), &SchKey1, &SchKey2, &SchKey3,&iv,DES_DECRYPT);
+        
+        /* Printing and Verifying */
+        printf("\nOriginal plain text : \n");
+        print(input_data,sizeof(input_data));
+        printf("\nEncrypted cipher text : \n");
+        print(cipher,sizeof(input_data));
+        printf("\nDecrypted plain text : \n");
+        print(text,sizeof(input_data));
+        printf("\n");
+        return 0;
+    }
 }
