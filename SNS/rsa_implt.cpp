5561d4dfbd6e65d3c61227c866f11300fca50f38
pritamksahoo <pritam.ndp@gmail.com>
2019-10-19 12:42:09 +0530

    SNS RSA Updates (GMP)

--- SNS/rsa_implt.cpp
+++ SNS/rsa_implt.cpp
@@ -65,6 +65,22 @@ int key_init(const mpz_t p, const mpz_t q, mpz_t n, const mpz_t enc_key, mpz_t d
 
 	mpz_inits(gcd, temp_p, temp_q, phi, NULL);
 
+	/* Checking primality of p and q */
+	int isPrimeP = mpz_probab_prime_p(p, 5);
+	int isPrimeQ = mpz_probab_prime_p(q, 5);
+
+	if (isPrimeP == 0)
+	{
+		cout << "\n\nERROR : P should be prime!\n\n---Program terminated---" << endl;
+		return 0;
+	}
+
+	if (isPrimeQ == 0)
+	{
+		cout << "\n\nERROR : Q should be prime!\n\n---Program terminated---" << endl;
+		return 0;
+	}
+
 	/* Compute value of n */
 	mpz_mul(n, p, q);
 
