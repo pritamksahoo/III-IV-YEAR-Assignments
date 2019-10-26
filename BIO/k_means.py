aa30c67fa5fe9c5bda885f68b7db72a62806a14a
pritamksahoo <pritam.ndp@gmail.com>
2019-10-26 21:01:02 +0530

    BIO

--- BIO/k_means.py
+++ BIO/k_means.py
@@ -1,6 +1,8 @@
+dim = 5
+
 def eucledian_dst(p1, p2):
 	dst = 0
-
+	# print(p1, p2, end=' ')
 	for index, val in enumerate(p1):
 		dst = dst + (val - p2[index])**2
 
@@ -12,6 +14,7 @@ def k_mean(no_points, points_in_5D, no_clusters, origins):
 	clusters = [0]*no_points
 
 	while change == True:
+		# Finding clusters in which points will fit into
 		for index_point, point in enumerate(points_in_5D):
 			min_dst, closest_org = 1000000000, 0 
 			for index_org, org in enumerate(origins):
@@ -21,25 +24,31 @@ def k_mean(no_points, points_in_5D, no_clusters, origins):
 					min_dst = dst
 					closest_org = index_org
 
+			# No cange
 			if clusters[index_point] == closest_org:
 				change = False
 			else:
+				# Change
 				clusters[index_point] = closest_org
 				change = True
 
-		origins = [[0, 0]]*no_clusters
+		# Updating origins
+		# origins = [[0]*dim]*no_clusters
+		origins = [[0 for i in range(dim)] for j in range(no_clusters)]
 
 		for index_cluster, cluster in enumerate(clusters):
-			origins[cluster] = [origins[cluster][0]+points_in_5D[index_cluster][0], origins[cluster][1]+points_in_5D[index_cluster][1]]
+			for i in range(dim):
+				origins[cluster][i] = origins[cluster][i]+points_in_5D[index_cluster][i]
 
 		# print("origins", origins, "clusters", clusters)
 
 		for cluster in range(no_clusters):
 			cnt = clusters.count(cluster)
 			if cnt != 0:
-				origins[cluster] = [origins[cluster][0]/cnt, origins[cluster][1]/cnt]
+				for i in range(dim):
+					origins[cluster][i] = origins[cluster][i]/cnt
 
-		# print(clusters, origins, '\n')
+		print(clusters, origins, '\n')
 
 	return clusters, origins
 
